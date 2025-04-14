764b22ddc4da22a5bc32dfa386d06ec59d29a85d
Dillon H. <181585418+Velarken@users.noreply.github.com>
2025-04-13 22:18:09 -0400

    added basic display function and event listeners for buttons

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Essential Math Operations
 function add(firstNumber, secondNumber) {
     if (firstNumber || secondNumber == NaN) {
         firstNumber = Number(firstNumber);
@@ -42,25 +43,76 @@ function operate(operator, firstNumber, secondNumber) {
     }
 }
 // Declared Variables
-let expressionTotal = 0;
-let inputExpression = [1,2,3,"/",2];
+let inputExpression = 0;
+let outputExpression = "";
+let firstInput = [];
+let operatorInput = "";
+let secondInput = [];
+
 // Constructors
-document.querySelector("#inputExpression").textContent = inputExpression;
-document.querySelector("#expressionTotal").textContent = expressionTotal;
+document.querySelector("#input").textContent = inputExpression;
+let output = document.querySelector("#output");
 
+// Numerical Event Listeners
+const buttonOne = document.querySelector('.one').addEventListener('click', () => display(1));
+const buttonTwo = document.querySelector('.two').addEventListener('click', () => display(2));
+const buttonThree = document.querySelector('.three').addEventListener('click', () => display(3));
+const buttonFour = document.querySelector('.four').addEventListener('click', () => display(4));
+const buttonFive = document.querySelector('.five').addEventListener('click', () => display(5));
+const buttonSix = document.querySelector('.six').addEventListener('click', () => display(6));
+const buttonSeven = document.querySelector('.seven').addEventListener('click', () => display(7));
+const buttonEight = document.querySelector('.eight').addEventListener('click', () => display(8));
+const buttonNine = document.querySelector('.nine').addEventListener('click', () => display(9));
+const buttonZero = document.querySelector('.zero').addEventListener('click', () => display(0));
 
-// Event Listeners
+// Operator Event Listeners
+const plusOperator = document.querySelector(".plus").addEventListener('click', () => display('+'));
+const minusOperator = document.querySelector(".minus").addEventListener('click', () => display('-'));
+const multiplyOperator = document.querySelector(".multiply").addEventListener('click', () => display('*'));
+const divideOperator = document.querySelector(".divide").addEventListener('click', () => display('/'));
+const equalsOperator = document.querySelector(".equals").addEventListener('click', () => display('='));
+const clear = document.querySelector(".clear").addEventListener('click', () => display("CLEAR"));
 
 // display function
-function display() {
-    //clicking a number button concats the clicked number to the end of the string
-        //when an operator is clicked 
-            // convert the string to a number
-            // store number inside variable "firstNumber"
-            // store operator inside variable "operator"
-            // begin concat of new string
-            // when enter is pressed
-                //convert string to number
-                //store number inside variable "secondNumber"
-                //evaluate expression given
+function display(clicked) {
+    console.log(clicked);
+    
+        if (clicked == '+') {
+            operatorInput = '+';
+        } else if (clicked == '-') {
+            operatorInput = '-';
+        } else if (clicked == '*') {
+            operatorInput = '*';
+        } else if (clicked == '/') {
+            operatorInput = '/';
+        } else {
+            firstInput.push(clicked);
+        }
+        setDisplay();
+        // display created string    
 };
+
+function setDisplay() {
+    let stringInput = firstInput.toString().replace(',', '');
+    output.textContent = stringInput;  
+}
+
+// KNOWN ISSUES
+/* ADDING MORE THAN 2 NUMBERS KEEPS THE COMMAS IN THE DISPLAY STATEMENT */
+
+
+
+    // if clicked button is a number
+        // concat number to end of string
+        // set input.textContent to string value
+    // if clicked button is an operator
+        // convert string to number, stored in "firstNumber"
+        // store operator in "operator"
+        // set output.textContent to string(firstNumber) and operator
+        // begin new empty string on input.textContent
+    // if clicked button is the equals key
+        // convert string to number, stored in "secondNumber"
+        // set input.textContent = firstNumber + operator + secondNumber
+        // run operation function on firstNumber, operator, secondNumber
+        // set output.textContent to the result of the function
+
